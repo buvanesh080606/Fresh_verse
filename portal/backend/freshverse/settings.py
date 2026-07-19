7368c10b1ccdc12bdf6265df98c0430ea464be1f
buvanesh080606 <vsbuvaneshraj06@gmail.com>
2026-07-19 21:44:38 +0530

    fix: add EMAIL_USE_SSL support for port 465 on Render

--- portal/backend/freshverse/settings.py
+++ portal/backend/freshverse/settings.py
@@ -215,6 +215,7 @@ if EMAIL_HOST:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
     EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
     EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 else:
